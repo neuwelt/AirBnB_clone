960c32040767ca52da1a542c11a0246ca9506d89
neuwelt <ineuwelt@neuwelt.co>
2022-10-29 11:39:37 +0000

    delete copy file

--- models/base_model.py
+++ models/base_model.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
+
 """BaseModel module"""
 import uuid
 import datetime
 import models
 
-
 class BaseModel:
     """class BaseModel"""
 
     def __init__(self, *args, **kwargs):
-        """__init__ method for BaseModel class
-        Args:
-            args (tuple): arguments
-            kwargs (dict): key word arguments
+
         """
+        __init__ method for BaseModel class
+            args (tuple): arguments
+            kwargs (dict): keyword arguments
+        """
+
         if kwargs:
             for name, value in kwargs.items():
                 if name != '__class__':
                     if name == 'created_at' or name == 'updated_at':
-                        value = datetime.datetime.strptime(
-                            value, "%Y-%m-%dT%H:%M:%S.%f")
+                        value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, name, value)
+
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
@@ -28,25 +30,27 @@ class BaseModel:
             models.storage.new(self)
 
     def __str__(self):
-        """string representation of BaseModel"""
-        return "[{}] ({}) {}".format(
-            type(self).__name__, self.id, self.__dict__)
+        """ string representation of BaseModel
+        """
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        """save method of BaseModel updates the public instance attribute
-        updated_at with the current datetime
+        """ save mothode of BaseModel updates the public instance
+            attribute updated_at with the current datetime
         """
         self.updated_at = datetime.datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """to_dict method of BaseModel creates dict with all keys/values of
-        __dict__ of the instance
-        Returns:
-            dictionary of instance key-value pairs
+        """ to_dict methode of BaseModel creates dict with all keys/values
+            of the __dict__ instance
+            Returns:
+                dictionary of instance key-values pairs
         """
         base_dict = dict(self.__dict__)
-        base_dict['__class__'] = type(self).__name__
-        base_dict['created_at'] = base_dict['created_at'].isoformat()
-        base_dict['updated_at'] = base_dict['updated_at'].isoformat()
+        base.dict['__class__'] = type(self).__name__
+        base.dict['created_at'] = base_dict['created_at'].isoformat()
+        base.dict['updated_at'] = base_dict['updated_at'].isoformat()
         return base_dict
+
+
